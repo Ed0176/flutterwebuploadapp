e4d74460062f2576988f2ffa8d5ae5512ac73289
FlutLab.io <>
2022-06-23 11:22:34 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:upload/edit_course.dart';
 
 //void main() => runApp(MyApp());
 
 main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(MyApp());
+  runApp(EditCourse());
 }
 
 class MyApp extends StatelessWidget {
